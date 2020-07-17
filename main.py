96a6cff061abac1156c238f007c288aa2c2d4b65
GeekyAmit5 <amitmishra95820@gmail.com>
2020-07-17 11:58:07 +0530

    player,food, lobby

--- main.py
+++ main.py
@@ -4,128 +4,104 @@ import math
 import random
 
 
-class blob:
-    def __init__(self):
-        self.x = 500
-        self.y = 300
-        self.radius = 30
+class blob(object):
+    def __init__(self, x, y, radius, color, speed=0):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.speed = speed
+        self.dir = (0, 0)
         self.area = math.pi * self.radius**2
-        self.color = red
 
-    def show(self):
+    def update_radius(self):
         self.radius = int(math.sqrt(self.area / math.pi))
-        pygame.draw.circle(win, self.color, (500, 300), self.radius)
 
-
-class food:
-    def __init__(self):
-        self.x = random.randint(-1910, 1910)
-        self.y = random.randint(-1080, 1080)
-        self.radius = random.randint(1, 5)
+    def update_area(self):
         self.area = math.pi * self.radius**2
-        self.color = random.choice([white, black, red, green, blue, yellow])
 
     def show(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
-
-
-class bot:
-    def __init__(self):
-        self.x = random.randint(-1900, 1900)
-        self.y = random.randint(-1050, 1050)
-        self.radius = random.randint(10, 50)
-        self.area = math.pi * self.radius**2
-        self.color = random.choice([white, black, red, green, blue, yellow])
-
-    def show(self):
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
-
-
-def isCollide(f, b, c):
-    if (b.x-f.x)**2 + (b.y-f.y)**2 < c:
-        return True
-    return False
 
 
 def main():
-    global bgx, bgy
-    b1 = blob()
+    player = blob(500, 300, 30, red, 5)
+    posx = player.x
+    posy = player.y
     foods = []
     for i in range(1000):
-        foods.append(food())
-    bots = []
-    for i in range(30):
-        bots.append(bot())
+        foods.append(blob(random.randint(inix, endx), random.randint(
+            iniy, endy), random.randint(2, 5), random.choice(colors)))
     while True:
-        win.blit(bg, (bgx, bgy))
-        b1.show()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         mx, my = pygame.mouse.get_pos()
-        if mx < 450 and bgx < 450 - b1.radius:
-            changex = speed
-        elif mx > 550 and bgx > -3350 + b1.radius:
-            changex = -speed
+        x = mx - player.x
+        y = my - player.y
+        if x**2+y**2 > player.radius:
+            dir = (x/math.sqrt(x**2+y**2), y/math.sqrt(x**2+y**2))
         else:
-            changex = 0
-
-        if my < 250 and bgy < 250 - b1.radius:
-            changey = speed
-        elif my > 350 and bgy > -1850 + b1.radius:
-            changey = -speed
+            dir = (0, 0)
+        win.fill(black)
+        pygame.draw.rect(win, white, (inix-posx+player.x, iniy-posy+player.y,
+                                      endx-inix, endy-iniy), 5)
+        if inix <= posx <= endx and iniy <= posy <= endy:
+            posx += int(dir[0]*player.speed)
+            posy += int(dir[1]*player.speed)
+            for food in foods:
+                food.x -= int(dir[0]*player.speed)
+                food.y -= int(dir[1]*player.speed)
+                if (food.x-player.x)**2+(food.y-player.y)**2 < player.radius**2-food.radius**2:
+                    food.x = random.randint(
+                        inix-posx+player.x, endx-posx+player.x)
+                    food.y = random.randint(
+                        iniy-posy+player.y, endy-posy+player.y)
+                    player.area += food.area
+                    player.update_radius()
+                food.show()
         else:
-            changey = 0
-
-        bgx += changex
-        bgy += changey
-        for f in foods:
-            f.show()
-            f.x += changex
-            f.y += changey
-            if (500-f.x)**2 + (300-f.y)**2 < b1.radius**2:
-                b1.area += f.area
-                f.x = random.randint(-1910, 1910)
-                f.y = random.randint(-1080, 1080)
-            for b in bots:
-                if isCollide(f, b, b.radius**2):
-                    b.area += f.area
-                    f.x = random.randint(-1910, 1910)
-                    f.y = random.randint(-1080, 1080)
-
-        for b in bots:
-            b.x += changex
-            b.y += changey
-            b.show()
-            b.x += random.choice([speed, 0, -speed])
-            b.y += random.choice([speed, 0, -speed])
-            if isCollide(b1, b, 300):
-                b1.area += b.area
-                b.x = random.randint(-1910, 1910)
-                b.y = random.randint(-1080, 1080)
-
+            if posx < inix:
+                posx = inix
+                for food in foods:
+                    food.x += int(dir[0]*player.speed)
+            elif posx > endx:
+                posx = endx
+                for food in foods:
+                    food.x += int(dir[0]*player.speed)
+            if posy < iniy:
+                posy = iniy
+                for food in foods:
+                    food.y += int(dir[1]*player.speed)
+            elif posy > endy:
+                posy = endy
+                for food in foods:
+                    food.y += int(dir[1]*player.speed)
+        player.show()
         win.blit(pygame.font.SysFont(
-            None, 30).render("Score: "+str(int(b1.area)//2-1400), True, white), [10, 10])
+            None, 30).render("Score: "+str(int(player.area)//5), True, white), [10, 10])
+        win.blit(pygame.font.SysFont(
+            None, int(4*player.radius/(len(name)))).render(name, True, white), [500-int(player.radius*0.825), 300-int(player.radius*(2/len(name)))])
         Clock.tick(fps)
         pygame.display.update()
 
 
-# imgae = 3840 * 2160
 pygame.init()
 win = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Agario")
-bg = pygame.image.load("assets/images/bg.jpg")
-bgx, bgy = (-1420, -580)
 white = (255, 255, 255)
 black = (0, 0, 0)
 green = (51, 204, 89)
 red = (250, 51, 51)
 blue = (50, 50, 250)
 yellow = (250, 250, 50)
-speed = 5
+colors = [white, red, blue, green, yellow]
 fps = 60
 Clock = pygame.time.Clock()
-
-
+inix = -500
+iniy = -500
+endx = 1500
+endy = 1100
+name = "AMIT MISHRA"
 main()
